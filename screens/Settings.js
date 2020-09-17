c5a2fe3404743f9f9f7dfb93022ba09033e739d3
KyungA <nek1717@naver.com>
2020-09-17 23:00:41 +0900

    캘린더 day클릭시 모달창 구현

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ScrollWrap from "../components/ScrollWrap";
 import { View, Text, Button } from "react-native";
-import ModalContainer from "../components/Modal";
 
 export default ({ loading }) => (
   <ScrollWrap loading={loading}>
     <View>
-      <ModalContainer />
+      <Text>설정화면</Text>
     </View>
   </ScrollWrap>
 );
